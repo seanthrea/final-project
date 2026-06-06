0049ce4f218ed6e3e2caec4b58a50874ab783361
Sean Miguel <Seanhanraymiguel@gmail.com>
2026-06-06 20:20:27 +0800

    Added UI and functionalities
    Added
    Main Menu
    Pause Menu
    Difficulty levels
    Win Panel
    Game Over Panel

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_easy_pressed():
+	GameManager.difficulty = "easy"
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_hard_pressed():
+	GameManager.difficulty = "hard"
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_meduim_pressed() -> void:
+	GameManager.difficulty = "medium"
+	get_tree().change_scene_to_file("res://main.tscn")
